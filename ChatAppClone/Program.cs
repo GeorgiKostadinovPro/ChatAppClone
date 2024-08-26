9734f4199227d6e95ffc955623af44fd7604cb66
GeorgiKostadinovPro <kostadinovgeorgi16@gmail.com>
2024-08-26 23:12:07 +0300

    Fix UserManager in LoginPartial file

--- ChatAppClone/Program.cs
+++ ChatAppClone/Program.cs
@@ -1,7 +1,6 @@
 using ChatAppClone.Data;
 using ChatAppClone.Data.Models;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 

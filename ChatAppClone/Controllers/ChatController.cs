a3abf7dffd0ce8ca9e92acc6eaf28270cb3e48b7
GeorgiKostadinovPro <kostadinovgeorgi16@gmail.com>
2024-08-29 21:13:39 +0300

    Add Chat page layout

---
+++ ChatAppClone/Controllers/ChatController.cs
@@ -0,0 +1,15 @@
+﻿namespace ChatAppClone.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class ChatController : BaseController
+    {
+        public ChatController() {}
+
+        [HttpGet]
+        public IActionResult Chat()
+        {
+            return this.View();
+        }
+    }
+}

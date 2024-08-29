efb10d59e723bf0eb79568ccca4a687748e8a7da
GeorgiKostadinovPro <kostadinovgeorgi16@gmail.com>
2024-08-29 21:58:50 +0300

    Finish Chat Page Layout and Design

--- ChatAppClone/wwwroot/js/chat.js
+++ ChatAppClone/wwwroot/js/chat.js
@@ -1,6 +1,8 @@
 ﻿const toggleButton = document.querySelector('.dark-light');
 const colors = document.querySelectorAll('.color');
 const appDivElement = document.querySelector('.app');
+const headerElement = document.querySelector('.site-header');
+const footerElement = document.querySelector('.site-footer');
 
 colors.forEach(color => {
     color.addEventListener('click', e => {
@@ -13,4 +15,6 @@ colors.forEach(color => {
 
 toggleButton.addEventListener('click', () => {
     appDivElement.classList.toggle('dark-mode');
+    headerElement.classList.toggle('dark-mode');
+    footerElement.classList.toggle('dark-mode');
 });

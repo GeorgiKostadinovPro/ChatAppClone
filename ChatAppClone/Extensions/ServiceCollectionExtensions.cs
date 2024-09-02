6b4a51ccee26670340b4d7a573c493489a6fee6a
GeorgiKostadinovPro <kostadinovgeorgi16@gmail.com>
2024-09-02 17:45:19 +0300

    Add IServiceCollection Extension Method

---
+++ ChatAppClone/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace ChatAppClone.Extensions
+{
+    using CloudinaryDotNet;
+
+    using Microsoft.Extensions.Configuration;
+
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection Add(this IServiceCollection services, IConfiguration configuration)
+        {
+            Account account = new Account(
+                configuration["Cloudinary:CloudName"], 
+                configuration["Cloudinary:APIKey"],
+                configuration["Cloudinary:APISecret"]);
+
+            Cloudinary cloudinary = new Cloudinary(account);
+
+            services.AddSingleton(cloudinary);
+            
+            return services;
+        }
+    }
+}

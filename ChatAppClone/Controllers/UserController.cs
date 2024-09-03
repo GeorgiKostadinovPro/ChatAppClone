b179c20236f7337d5f50776112afcccab68ee92e
GeorgiKostadinovPro <kostadinovgeorgi16@gmail.com>
2024-09-03 17:35:56 +0300

    Finish Set profile picture functionality

--- ChatAppClone/Controllers/UserController.cs
+++ ChatAppClone/Controllers/UserController.cs
@@ -4,14 +4,20 @@
 
     using ChatAppClone.Utilities.Contracts;
     using ChatAppClone.Common.Constants;
+    using CloudinaryDotNet.Actions;
+    using ChatAppClone.Core.Contracts;
 
     public class UserController : BaseController
     {
         private readonly ICloudinaryService cloudinaryService;
 
-        public UserController(ICloudinaryService _cloudinaryService)
+        private readonly IUserService userService;
+
+        public UserController(ICloudinaryService _cloudinaryService, IUserService _userService)
         {
             this.cloudinaryService = _cloudinaryService;
+
+            this.userService = _userService;
         }
 
         [HttpPost]
@@ -27,10 +33,17 @@
                 return this.Json(new { success = false, error = "The valid types are .jpg, .jpeg, .png" });
             }
 
-            // Your file processing logic here
-            await this.cloudinaryService.UploadPictureAsync(file, CloudinaryConstants.ProfilePicturesFolder);
+            try
+            {
+                ImageUploadResult result = await this.cloudinaryService.UploadPictureAsync(file, CloudinaryConstants.ProfilePicturesFolder);
 
-            // Return success message as JSON
+                await this.userService.SetUserProfilePictureAsync(this.GetAuthId(), result.SecureUrl.ToString(), result.PublicId);
+            }
+            catch (Exception ex)
+            {
+                return this.Json(new { success = false, error = ex.Message });
+            }
+
             return this.Json(new { success = true });
         }
     }
--- ChatAppClone/Core/UserService.cs
+++ ChatAppClone/Core/UserService.cs
@@ -1,8 +1,39 @@
 ﻿namespace ChatAppClone.Core
 {
     using ChatAppClone.Core.Contracts;
+    using ChatAppClone.Data.Models;
+    using Microsoft.AspNetCore.Identity;
+    using System.Threading.Tasks;
 
     public class UserService : IUserService
     {
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public UserService(UserManager<ApplicationUser> _userManager)
+        {
+            this.userManager = _userManager;
+        }
+
+        public async Task<ApplicationUser> GetUserByIdAsync(string userId)
+        {
+            ApplicationUser user = await userManager.FindByIdAsync(userId);
+
+            if (user == null)
+            {
+                throw new InvalidOperationException("User with such id does not exist.");
+            }
+
+            return user;
+        }
+
+        public async Task SetUserProfilePictureAsync(string userId, string url, string publicId)
+        {
+            ApplicationUser user = await this.GetUserByIdAsync(userId);
+
+            user.ProfilePictureUrl = url;
+            user.ProfilePicturePublicId = publicId;
+
+            await userManager.UpdateAsync(user);
+        }
     }
 }

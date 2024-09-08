755a66b5f00e2fa904f8a921888943d9692fc794
GeorgiKostadinovPro <kostadinovgeorgi16@gmail.com>
2024-09-08 15:36:03 +0300

    Fix layout upon deleting picture

--- ChatAppClone/wwwroot/js/profilePictureUploadDelete.js
+++ ChatAppClone/wwwroot/js/profilePictureUploadDelete.js
@@ -32,16 +32,6 @@
         });
     });
 
-    $('#remove-button').on('click', function (e) {
-        e.preventDefault();
-
-        $('#removePictureModal').modal('show');
-    });
-
-    $('#removePictureModal .close').on('click', function () {
-        $('#removePictureModal').modal('hide');
-    });
-
     $('#remove-profile-picture-btn').on('click', function (e) {
         e.preventDefault();
 
@@ -68,5 +58,23 @@
                 $('#loading-overlay').hide();
             }
         });
-    })
+    });
+
+    $('#remove-button').on('click', function (e) {
+        e.preventDefault();
+
+        $('#removePictureModal').modal('show');
+    });
+
+    $('#removePictureModal .close').on('click', function () {
+        $('#removePictureModal').modal('hide');
+    });
+
+    document.getElementById('removePictureModal').addEventListener('show.bs.modal', function () {
+        document.body.classList.add('modal-open');
+    });
+
+    document.getElementById('removePictureModal').addEventListener('hidden.bs.modal', function () {
+        document.body.classList.remove('modal-open');
+    });
 });

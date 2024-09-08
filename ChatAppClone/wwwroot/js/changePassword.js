18ae3a355b5e3108467e1ee8e1a3b3b18f3da6e5
GeorgiKostadinovPro <kostadinovgeorgi16@gmail.com>
2024-09-08 15:08:31 +0300

    Reorginaze js scripts

---
+++ ChatAppClone/wwwroot/js/changePassword.js
@@ -0,0 +1,16 @@
+﻿const cancelBtnElement = document.querySelector('.cancel-btn');
+
+cancelBtnElement.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const inputElements = document.querySelectorAll('#change-password-form input');
+    const spanElements = document.querySelectorAll('#change-password-form span');
+
+    inputElements.forEach((input) => {
+        input.value = '';
+    });
+
+    spanElements.forEach((span) => {
+        span.style.display = 'none';
+    });
+});
---
+++ ChatAppClone/wwwroot/js/profilePictureUploadDelete.js
@@ -0,0 +1,72 @@
+﻿$(document).ready(function () {
+    $('#upload-button').on('click', function (e) {
+        e.preventDefault();
+
+        var fileInput = $('#profile-picture-input')[0];
+        var file = fileInput.files[0];
+
+        var formData = new FormData();
+        formData.append('file', file);
+
+        $('#loading-overlay').show();
+
+        $.ajax({
+            url: '@Url.Action("UploadProfilePicture", "User")',
+            type: 'POST',
+            data: formData,
+            processData: false,
+            contentType: false,
+            success: function (response) {
+                if (response.success) {
+                    location.reload();
+                } else {
+                    $('#validation-message').text(response.error);
+                }
+            },
+            error: function () {
+                $('#validation-message').text('An error occurred while uploading the picture.');
+            },
+            complete: function () {
+                $('#loading-overlay').hide();
+            }
+        });
+    });
+
+    $('#remove-button').on('click', function (e) {
+        e.preventDefault();
+
+        $('#removePictureModal').modal('show');
+    });
+
+    $('#removePictureModal .close').on('click', function () {
+        $('#removePictureModal').modal('hide');
+    });
+
+    $('#remove-profile-picture-btn').on('click', function (e) {
+        e.preventDefault();
+
+        $('#removePictureModal').modal('hide');
+
+        $('#loading-overlay').show();
+
+        $.ajax({
+            url: '@Url.Action("DeleteProfilePicture", "User")',
+            type: 'POST',
+            processData: false,
+            contentType: false,
+            success: function (response) {
+                if (response.success) {
+                    location.reload();
+                } else {
+                    $('#validation-message').text(response.error);
+                }
+            },
+            error: function () {
+                $('#validation-message').text('An error occurred while deleting the picture.');
+            },
+            complete: function () {
+                $('#loading-overlay').hide();
+            }
+        });
+    })
+});

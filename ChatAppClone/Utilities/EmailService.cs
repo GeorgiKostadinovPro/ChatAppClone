c8ffbbc259b4b6c98f7caea1da6a61583f8f789a
GeorgiKostadinovPro <kostadinovgeorgi16@gmail.com>
2024-09-11 15:58:23 +0300

    Implement Email Sender Service

---
+++ ChatAppClone/Utilities/EmailService.cs
@@ -0,0 +1,33 @@
+﻿namespace ChatAppClone.Utilities
+{
+    using ChatAppClone.Utilities.Contracts;
+    using MailKit.Net.Smtp;
+    using MailKit.Security;
+    using MimeKit;
+    using MimeKit.Text;
+
+    public class EmailService : IEmailService
+    {
+        private readonly IConfiguration configuration;
+
+        public EmailService(IConfiguration _configuration)
+        {
+            this.configuration = _configuration;
+        }
+
+        public void Send(string to, string subject, string html, string? from = null)
+        {
+            MimeMessage email = new MimeMessage();
+            email.From.Add(MailboxAddress.Parse(from ?? this.configuration["MailKit:FromEmail"]));
+            email.To.Add(MailboxAddress.Parse(to));
+            email.Subject = subject;
+            email.Body = new TextPart(TextFormat.Html) { Text = html };
+
+            using SmtpClient smtp = new SmtpClient();
+            smtp.Connect(this.configuration["MailKit:Host"], int.Parse(this.configuration["MailKit:Port"]), SecureSocketOptions.StartTls);
+            smtp.Authenticate(this.configuration["MailKit:Username"], this.configuration["MailKit:Password"]);
+            smtp.Send(email);
+            smtp.Disconnect(true);
+        }
+    }
+}

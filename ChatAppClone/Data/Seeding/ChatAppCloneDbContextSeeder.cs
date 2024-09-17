9884cc76b4d36255964c628b3c3f03ed5bbe4445
GeorgiKostadinovPro <kostadinovgeorgi16@gmail.com>
2024-09-17 18:24:07 +0300

    Integrate Data Seeder Service

---
+++ ChatAppClone/Data/Seeding/ChatAppCloneDbContextSeeder.cs
@@ -0,0 +1,36 @@
+﻿namespace ChatAppClone.Data.Seeding
+{
+    using ChatAppClone.Data.Seeding.Contracts;
+    using System;
+    using System.Threading.Tasks;
+
+    public class ChatAppCloneDbContextSeeder : ISeeder
+    {
+        public async Task SeedAsync(ChatAppCloneDbContext dbContext, IServiceProvider serviceProvider)
+        {
+            if (dbContext == null)
+            {
+                throw new ArgumentNullException(nameof(dbContext));
+            }
+
+            if (serviceProvider == null)
+            {
+                throw new ArgumentNullException(nameof(serviceProvider));
+            }
+
+            var logger = serviceProvider.GetService<ILoggerFactory>()?.CreateLogger(typeof(ChatAppCloneDbContextSeeder));
+
+            var seeders = new List<ISeeder>
+                          {
+                            new UsersSeeder()
+                          };
+
+            foreach (var seeder in seeders)
+            {
+                await seeder.SeedAsync(dbContext, serviceProvider);
+                await dbContext.SaveChangesAsync();
+                logger?.LogInformation($"Seeder {seeder.GetType().Name} done.");
+            }
+        }
+    }
+}
---
+++ ChatAppClone/Data/Seeding/UsersSeeder.cs
@@ -0,0 +1,50 @@
+﻿namespace ChatAppClone.Data.Seeding
+{
+    using ChatAppClone.Data.Models;
+    using ChatAppClone.Data.Seeding.Contracts;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.Threading.Tasks;
+
+    public class UsersSeeder : ISeeder
+    {
+        public async Task SeedAsync(ChatAppCloneDbContext dbContext, IServiceProvider serviceProvider)
+        {
+            if (await dbContext.Users.AnyAsync())
+            {
+                return;
+            }
+
+            UserManager<ApplicationUser> userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+            if (userManager == null)
+            {
+                throw new ArgumentNullException(nameof(userManager));
+            }
+
+            ApplicationUser user1 = new ApplicationUser
+            {
+                UserName = "Go4ko",
+                Email = "georgi.kostadinov14@abv.bg",
+                EmailConfirmed = true,
+                PasswordHash = "Gk123.",
+                ProfilePictureUrl = "https://res.cloudinary.com/de1i8aava/image/upload/v1726509898/ChatAppClone/assets/user-profile-pictures/profile-picture.jpg",
+                ProfilePicturePublicId = "ChatAppClone/assets/user-profile-pictures/profile-picture"
+            };
+
+            ApplicationUser user2 = new ApplicationUser
+            {
+                UserName = "Lyub4o",
+                Email = "user@mail.com",
+                EmailConfirmed = true,
+                PasswordHash = "Gk123.",
+                ProfilePictureUrl = "https://res.cloudinary.com/de1i8aava/image/upload/v1725385674/ChatAppClone/assets/user-profile-pictures/profil1.jpg",
+                ProfilePicturePublicId = "ChatAppClone/assets/user-profile-pictures/profil1"
+            };
+
+            await userManager.CreateAsync(user1);
+            await userManager.CreateAsync(user2);
+        }
+    }
+}

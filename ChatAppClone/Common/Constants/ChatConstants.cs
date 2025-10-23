47327a54f49ed3e290543f02de95cb39867d77c4
GeorgiKostadinovPro <kostadinovgeorgi16@gmail.com>
2025-10-23 11:52:51 +0300

    Refactor ChatService

--- ChatAppClone/Common/Constants/ChatConstants.cs
+++ ChatAppClone/Common/Constants/ChatConstants.cs
@@ -5,5 +5,7 @@
         public const string DefaultChatImage = "https://imgs.search.brave.com/zrEc3_-7gHjjisfcBt0HMyNkrr4Ql8lWyk8eLAS8qBk/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4t/aWNvbnMtcG5nLmZs/YXRpY29uLmNvbS8x/MjgvMTQwMzQvMTQw/MzQyNzYucG5n";
 
         public const string NoMessagesYet = "No messages yet.";
+
+        public const string ChatName = "{0} & {1}";
     }
 }

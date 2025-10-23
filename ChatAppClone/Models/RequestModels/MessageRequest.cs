3f2e966642fbc22e51fd91bf26fde3f4f271909d
GeorgiKostadinovPro <kostadinovgeorgi16@gmail.com>
2025-10-23 12:24:42 +0300

    Simplify Message Controller

--- ChatAppClone/Models/RequestModels/MessageRequest.cs
+++ ChatAppClone/Models/RequestModels/MessageRequest.cs
@@ -1,9 +1,14 @@
 ﻿namespace ChatAppClone.Models.RequestModels
 {
+    using System.ComponentModel.DataAnnotations;
+
     public class MessageRequest
     {
-        public Guid ChatId { get; set; }
+        [Required(ErrorMessage = "ChatId is required.")]
+        public Guid? ChatId { get; set; }
 
-        public string Message { get; set; }
+        [Required(ErrorMessage = "Message is required.")]
+        [StringLength(500, MinimumLength = 1, ErrorMessage = "Message must be between 1 and 500 characters.")]
+        public string? Message { get; set; }
     }
 }

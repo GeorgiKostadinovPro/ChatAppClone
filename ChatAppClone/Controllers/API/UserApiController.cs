826a87a88e0d157666a88eef94ac7714b8e158aa
GeorgiKostadinovPro <kostadinovgeorgi16@gmail.com>
2025-10-24 16:40:55 +0300

    Finish Middleware Exception Handling

--- ChatAppClone/Controllers/API/UserApiController.cs
+++ ChatAppClone/Controllers/API/UserApiController.cs
@@ -39,54 +39,40 @@
                 return this.BadRequest(new { success = false, error = UserMessages.ValidTypes });
             }
 
-            try
+            var currUserId = this.GetAuth().Item1;
+
+            if (string.IsNullOrWhiteSpace(currUserId))
             {
-                var currUserId = this.GetAuth().Item1;
-
-                if (string.IsNullOrWhiteSpace(currUserId))
-                {
-                    return this.RedirectToAction(GeneralPages.Error, GeneralPages.Home, new { statusCode = 401 });
-                }
-
-                ApplicationUser user = await this.userService.GetByIdAsync(currUserId);
-
-                ImageUploadResult result = await this.cloudinaryService
-                    .UploadPictureAsync(file, CloudinaryConstants.ProfilePicturesFolder, user.ProfilePicturePublicId);
-
-                await this.userService.SetProfilePictureAsync(currUserId, result.SecureUrl.ToString(), result.PublicId);
-
-                return this.Ok(new { success = true });
+                return this.RedirectToAction(GeneralPages.Error, GeneralPages.Home, new { statusCode = 401 });
             }
-            catch (Exception ex)
-            {
-                return this.BadRequest(new { success = false, error = ex.Message });
-            }
+
+            ApplicationUser user = await this.userService.GetByIdAsync(currUserId);
+
+            ImageUploadResult result = await this.cloudinaryService
+                .UploadPictureAsync(file, CloudinaryConstants.ProfilePicturesFolder, user.ProfilePicturePublicId);
+
+            await this.userService.SetProfilePictureAsync(currUserId, result.SecureUrl.ToString(), result.PublicId);
+
+            return this.Ok(new { success = true });
         }
 
         [HttpPost]
         public async Task<IActionResult> DeleteProfilePicture()
         {
-            try
+            var currUserId = this.GetAuth().Item1;
+            
+            if (string.IsNullOrWhiteSpace(currUserId))
             {
-                var currUserId = this.GetAuth().Item1;
-
-                if (string.IsNullOrWhiteSpace(currUserId))
-                {
-                    return this.RedirectToAction(GeneralPages.Error, GeneralPages.Home, new { statusCode = 401 });
-                }
-
-                ApplicationUser user = await this.userService.GetByIdAsync(currUserId);
-
-                await this.cloudinaryService.DeletePictureAsync(user.ProfilePicturePublicId!);
-
-                await this.userService.DeleteProfilePictureAsync(currUserId);
-
-                return this.Ok(new { success = true });
+                return this.RedirectToAction(GeneralPages.Error, GeneralPages.Home, new { statusCode = 401 });
             }
-            catch (Exception ex)
-            {
-                return this.BadRequest(new { success = false, error = ex.Message });
-            }
+            
+            ApplicationUser user = await this.userService.GetByIdAsync(currUserId);
+            
+            await this.cloudinaryService.DeletePictureAsync(user.ProfilePicturePublicId!);
+            
+            await this.userService.DeleteProfilePictureAsync(currUserId);
+            
+            return this.Ok(new { success = true });
         }
     }
 }
--- ChatAppClone/Controllers/MVC/ChatController.cs
+++ ChatAppClone/Controllers/MVC/ChatController.cs
@@ -38,71 +38,57 @@
         [HttpGet]
         public async Task<IActionResult> Start(string userToChatId)
         {
-            try
+            var (currUserId, currentUserName) = this.GetAuth();
+            
+            if (string.IsNullOrWhiteSpace(currUserId) || string.IsNullOrWhiteSpace(currentUserName))
             {
-                var (currUserId, currentUserName) = this.GetAuth();
-
-                if (string.IsNullOrWhiteSpace(currUserId) || string.IsNullOrWhiteSpace(currentUserName))
-                {
-                    return this.RedirectToAction(GeneralPages.Error, GeneralPages.Home, new { statusCode = 401 });
-                }
-
-                if (await this.chatService.CheckIfChatExists(currUserId, userToChatId))
-                {
-                    return this.RedirectToAction(ChatPages.Chats);
-                }
-
-                var chat = await this.chatService.CreateAsync(currUserId, userToChatId);
-
-                await this.notificationService
-                    .CreateAsync(string.Format(NotificationMessages.UserAddedYouToChat, currentUserName), string.Empty, userToChatId);
-
-                await this.notificationHub.Clients.User(userToChatId)
-                    .SendAsync(NotificationMessages.ReceiveNotification, string.Format(NotificationMessages.UserAddedYouToChat, currentUserName));
-
-                await this.chatHub.Clients.User(userToChatId).SendAsync(ChatMessages.StartChat, new
-                {
-                    id = chat.Id,
-                    name = chat.Name,
-                    imageUrl = chat.ImageUrl,
-                    lastActive = chat.LastActive,
-                    lastMessage = chat.LastMessage,
-                    participantIds = chat.Participants.Select(p => p.Id).ToArray()
-                });
-
+                return this.RedirectToAction(GeneralPages.Error, GeneralPages.Home, new { statusCode = 401 });
+            }
+            
+            if (await this.chatService.CheckIfChatExists(currUserId, userToChatId))
+            {
                 return this.RedirectToAction(ChatPages.Chats);
             }
-            catch (Exception)
+            
+            var chat = await this.chatService.CreateAsync(currUserId, userToChatId);
+            
+            await this.notificationService
+                .CreateAsync(string.Format(NotificationMessages.UserAddedYouToChat, currentUserName), string.Empty, userToChatId);
+            
+            await this.notificationHub.Clients.User(userToChatId)
+                .SendAsync(NotificationMessages.ReceiveNotification, string.Format(NotificationMessages.UserAddedYouToChat, currentUserName));
+            
+            await this.chatHub.Clients.User(userToChatId).SendAsync(ChatMessages.StartChat, new
             {
-                return this.RedirectToAction(GeneralPages.Error, GeneralPages.Home, new { statusCode = 400 });
-            }
+                id = chat.Id,
+                name = chat.Name,
+                imageUrl = chat.ImageUrl,
+                lastActive = chat.LastActive,
+                lastMessage = chat.LastMessage,
+                participantIds = chat.Participants.Select(p => p.Id).ToArray()
+            });
+            
+            return this.RedirectToAction(ChatPages.Chats);
         }
 
         [HttpGet]
         public async Task<IActionResult> Chats()
         {
-            try
+            var currUserId = this.GetAuth().Item1;
+            
+            if (string.IsNullOrWhiteSpace(currUserId))
             {
-                var currUserId = this.GetAuth().Item1;
-
-                if (string.IsNullOrWhiteSpace(currUserId))
-                {
-                    return this.RedirectToAction(GeneralPages.Error, GeneralPages.Home, new { statusCode = 401 });
-                }
-
-                var chats = await this.chatService.GetByUserAsync(currUserId);
-
-                var model = new GeneralChatViewModel
-                {
-                    Chats = chats
-                };
-
-                return this.View(model);
+                return this.RedirectToAction(GeneralPages.Error, GeneralPages.Home, new { statusCode = 401 });
             }
-            catch (Exception ex)
+            
+            var chats = await this.chatService.GetByUserAsync(currUserId);
+            
+            var model = new GeneralChatViewModel
             {
-                return this.RedirectToAction(GeneralPages.Error, GeneralPages.Home, new { statusCode = 400 });
-            }
+                Chats = chats
+            };
+            
+            return this.View(model);
         }
 
         [HttpGet]
@@ -113,25 +99,18 @@
                 return this.RedirectToAction(GeneralPages.Error, GeneralPages.Home, new { statusCode = 404 });
             }
 
-            try
+            var currUserId = this.GetAuth().Item1;
+            
+            if (string.IsNullOrWhiteSpace(currUserId))
             {
-                var currUserId = this.GetAuth().Item1;
-
-                if (string.IsNullOrWhiteSpace(currUserId))
-                {
-                    return this.RedirectToAction(GeneralPages.Error, GeneralPages.Home, new { statusCode = 401 });
-                }
-
-                ViewBag.CurrentUserId = currUserId;
-
-                var chatModel = await this.chatService.GetByIdAsync(chatId.Value);
-
-                return this.PartialView("_ChatDetailsPartial", chatModel);
+                return this.RedirectToAction(GeneralPages.Error, GeneralPages.Home, new { statusCode = 401 });
             }
-            catch (Exception)
-            {
-                return this.RedirectToAction(GeneralPages.Error, GeneralPages.Home, new { statusCode = 400 });
-            }
+            
+            ViewBag.CurrentUserId = currUserId;
+            
+            var chatModel = await this.chatService.GetByIdAsync(chatId.Value);
+            
+            return this.PartialView("_ChatDetailsPartial", chatModel);
         }
     }
 }

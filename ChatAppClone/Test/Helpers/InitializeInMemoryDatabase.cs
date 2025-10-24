1d4048092b59010871ceba58583d03a629ec7775
GeorgiKostadinovPro <kostadinovgeorgi16@gmail.com>
2025-10-24 18:51:08 +0300

    Implement Unit Test via NUnit and Moq

---
+++ ChatAppClone/Test/Helpers/InitializeInMemoryDatabase.cs
@@ -0,0 +1,23 @@
+﻿namespace ChatAppClone.Test.Helpers
+{
+    using Microsoft.EntityFrameworkCore;
+
+    using ChatAppClone.Data;
+
+    public static class InitializeInMemoryDatabase
+    {
+        public static async Task<ChatAppCloneDbContext> CreateInMemoryDatabase()
+        {
+            DbContextOptions<ChatAppCloneDbContext> _options = new DbContextOptionsBuilder<ChatAppCloneDbContext>()
+                      .UseInMemoryDatabase(databaseName: "TestDb")
+                      .Options;
+
+            ChatAppCloneDbContext dbContext = new ChatAppCloneDbContext(_options);
+
+            await dbContext.Database.EnsureDeletedAsync();
+            await dbContext.Database.EnsureCreatedAsync();
+
+            return dbContext;
+        }
+    }
+}
